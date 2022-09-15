bf8c3c22326ada1cba7e577d291850d5bfe0e7cd
JayAtSeneca <jpchaudhari@myseneca.ca>
2022-09-15 00:56:04 -0400

    coding part done, get requests working

--- assignment1/moviesApi/server.js
+++ assignment1/moviesApi/server.js
@@ -15,22 +15,52 @@ app.get("/", function (req, res) {
 
 //CREATE
 app.post("/api/movies", (req, res) => {
-    db.addNewMovie(req.body)
-    .then((data) => res.json(data))
-    .catch((err) => res.json({ message: err }));
+  db.addNewMovie(req.body)
+    .then((data) => res.status(201).json(data))
+    .catch((err) => res.status(500).json({ message: err }));
 });
 
 //READ
 app.get("/api/movies", (req, res) => {
-
+  if (req.query.page && req.query.perPage) {
     db.getAllMovies(req.query.page, req.query.perPage, req.query.title)
-    .then((data) => res.json(data))
-    .catch((err) => res.json({ message: err }));
+      .then((data) => res.json(data))
+      .catch((err) => res.status(500).json({ message: err.message }));
+  } else {
+    res.status(400).json({ message: "ERROR: Bad Request" });
+  }
+});
+
+//READ BY ID
+app.get("/api/movies/:id", (req, res) => {
+  if (req.params.id) {
+    db.getMovieById(req.params.id)
+      .then((data) => res.json(data))
+      .catch((err) => res.status(500).json({ message: err }));
+  }
+});
+
+//UPDATE
+app.put("/api/movies/:id", (req, res) => {
+  if (req.params.id) {
+    db.updateMovieById(req.body, req.params.id)
+      .then((data) => res.status(204).json({ message: "Updated Successfully" }))
+      .catch((err) => res.status(500).json({ message: err }));
+  }
+});
+
+//DELETE
+app.delete("/api/movies/:id", (req, res) => {
+  if (req.params.id) {
+    db.deleteMovieById(req.params.id)
+      .then((data) => res.status(204).json({ message: "Deleted Successfully" }))
+      .catch((err) => res.status(500).json({ message: err }));
+  }
 });
 
 app.use((req, res) => {
-    res.status(404).send('Resource not found');
-  });
+  res.status(404).send("Resource not found");
+});
 
 db.initialize(process.env.MONGODB_CONN_STRING)
   .then(() => {
