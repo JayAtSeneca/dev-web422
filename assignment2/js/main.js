115713e4045cd2b1abcc1ef7ee3a0438451a2c33
JayAtSeneca <jpchaudhari@myseneca.ca>
2022-09-30 10:50:08 -0400

    updated main.js

--- assignment2/js/main.js
+++ assignment2/js/main.js
@@ -1,3 +1,12 @@
+/*********************************************************************************
+* WEB422 – Assignment 2
+* I declare that this assignment is my own work in accordance with Seneca Academic Policy.
+* No part of this assignment has been copied manually or electronically from any other source
+* (including web sites) or distributed to other students.
+*
+* Name: Jay Pravinkumar Chaudhari Student ID: 147268205 Date: 2022-09-30
+*
+********************************************************************************/ 
 let page = 1;
 const perPage = 10;
 function loadMovieData(title = null) {
