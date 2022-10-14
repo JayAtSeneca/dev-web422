f3f4f99b0656f1e535ab5f81db1feb74f408cea9
JayAtSeneca <jpchaudhari@myseneca.ca>
2022-10-14 14:44:47 -0400

    updated index.js added credentials

--- assignment3/my-app/pages/index.js
+++ assignment3/my-app/pages/index.js
@@ -1,3 +1,13 @@
+/*********************************************************************************
+* WEB422 – Assignment 3
+* I declare that this assignment is my own work in accordance with Seneca Academic Policy.
+* No part of this assignment has been copied manually or electronically from any other source
+* (including web sites) or distributed to other students.
+*
+* Name: Jay Pravinkumar Chaudhari Student ID: 147268205 Date: 2022-10-14
+*
+*
+********************************************************************************/
 import useSWR from 'swr';
 import { useState, useEffect } from "react";
 import Pagination from 'react-bootstrap/Pagination';
