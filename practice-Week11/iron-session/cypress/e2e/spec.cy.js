4d0c3681063aa43df880bda7f04a802f8ce954d9
JayAtSeneca <jpchaudhari@myseneca.ca>
2022-11-05 22:23:49 -0400

    E2E (End to End) Testing using Cypress

---
+++ practice-Week11/iron-session/cypress/e2e/spec.cy.js
@@ -0,0 +1,25 @@
+// describe('empty spec', () => {
+//   it('passes', () => {
+//     cy.visit('https://example.cypress.io')
+//   })
+// })
+describe('login / logout flow specification', () => {
+  it('cannot navigate to /profile-sg without being logged in', () => {
+    cy.visit("/profile-sg")
+    .url().should('include', "/login");
+  });
+
+  it('rejects a login attempt by an invalid github user: !!!', () => {
+    cy.visit("/login")
+    .get('input[name="username"]').type("!!!").type("{enter}")
+    .url().should('include', "/login");
+  });
+
+  it('successfully authenticates a valid github user: test-account and logs out', () => {
+    cy.visit("/login")
+    .get('input[name="username"]').type("test-account").type("{enter}")
+    .url().should('include', '/profile-sg')
+    .get("nav").contains("Logout").click({force:true})
+    .url().should('include', "/login");
+  });
+});
