11b46edace2228febfcb45f3b238230388278e34
JayAtSeneca <jpchaudhari@myseneca.ca>
2022-11-13 23:01:44 -0500

    Step 2: Automatically Hiding the Expanded Navbar (Mobile / Small Screens) done

--- assignment5/my-app/components/MainNav.js
+++ assignment5/my-app/components/MainNav.js
@@ -10,23 +10,26 @@ import Link from 'next/link';
 
 export default function MainNav(){
     const [searchField, setSearchField] = useState('');
+    const [isExpanded, setIsExpanded] = useState(false);
     const router = useRouter();
 
     function submitForm(e){
         e.preventDefault();
+        setIsExpanded(false);
         router.push(`/artwork?title=true&q=${searchField}`);
     }
     return (
         <>
-        <Navbar className="fixed-top navbar-dark bg-dark" expand="lg">
+        <Navbar className="fixed-top navbar-dark bg-dark" expand="lg" expanded={isExpanded}>
           <Container fluid>
             <Navbar.Brand >Jay Pravinkumar Chaudhari</Navbar.Brand>
-            <Navbar.Toggle aria-controls="navbarScroll" />
+            <Navbar.Toggle aria-controls="navbarScroll" onClick={e=>setIsExpanded((value)=>!value)} />
             <Navbar.Collapse id="navbarScroll">
               <Nav className="me-auto">
-                <Link href="/" passHref legacyBehavior><Nav.Link>Home</Nav.Link></Link>
-                <Link href="/search" passHref legacyBehavior><Nav.Link>Advanced Search</Nav.Link></Link>
+                <Link href="/" passHref legacyBehavior><Nav.Link onClick={e=>setIsExpanded((value)=>!value)}>Home</Nav.Link></Link>
+                <Link href="/search" passHref legacyBehavior><Nav.Link onClick={e=>setIsExpanded((value)=>!value)}>Advanced Search</Nav.Link></Link>
               </Nav>
+              &nbsp;
               <Form className="d-flex" onSubmit={submitForm}>
                 <Form.Control
                   type="search"
@@ -38,6 +41,7 @@ export default function MainNav(){
                 />
                 <Button variant="success" type='submit'>Search</Button>
               </Form>
+              &nbsp;
             </Navbar.Collapse>
           </Container>
         </Navbar>
