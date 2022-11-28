a91b32fb70e46f185754db64f723877639e4aa3b
JayAtSeneca <jpchaudhari@myseneca.ca>
2022-11-27 22:02:33 -0500

    added passport-jwt strategy code

--- assignment6/user-api/server.js
+++ assignment6/user-api/server.js
@@ -10,7 +10,31 @@ const passportJWT = require("passport-jwt");
 const HTTP_PORT = process.env.PORT || 8080;
 
 
+// JSON Web Token Setup
+let ExtractJwt = passportJWT.ExtractJwt;
+let JwtStrategy = passportJWT.Strategy;
 
+// Configure its options
+let jwtOptions = {};
+jwtOptions.jwtFromRequest = ExtractJwt.fromAuthHeaderWithScheme('jwt');
+
+jwtOptions.secretOrKey = process.env.JWT_SECRET;
+
+let strategy = new JwtStrategy(jwtOptions, function (jwt_payload, next) {
+  console.log('payload received', jwt_payload);
+
+  if (jwt_payload) {
+    next(null, {
+      _id: jwt_payload._id,
+      userName: jwt_payload.userName,
+    });
+  } else {
+    next(null, false);
+  }
+});
+
+// tell passport to use our "strategy"
+passport.use(strategy);
 
 app.use(express.json());
 app.use(cors());
